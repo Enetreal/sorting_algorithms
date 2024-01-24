1e1c5cbf6996e8ea5a799107eeb44892356e5d16
Enetreal <wizzarddguru@gmail.com>
2024-01-24 19:27:37 +0100

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>  /* Include necessary standard header for size_t */
 
 /* Comparison direction macros for bitonic sort */
 #define UP 0
@@ -15,8 +14,8 @@
  */
 typedef enum bool
 {
-	false = 0,
-	true
+    false = 0,
+    true
 } bool;
 
 /**
@@ -28,16 +27,16 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms */
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
