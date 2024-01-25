acb83e3d1646d7c40c5b7a086d23143a27672de8
Enetreal <wizzarddguru@gmail.com>
2024-01-25 10:45:49 +0100

    sorting_algoritms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void merge(int *array, int *auxiliary, size_t start, size_t mid, size_t end);
-
 /**
  * merge - Merges two subarrays in ascending order.
  * @array: The main array containing subarrays to be merged.
@@ -12,13 +10,11 @@ void merge(int *array, int *auxiliary, size_t start, size_t mid, size_t end);
  * @mid: The ending index of the first subarray (exclusive).
  * @end: The ending index of the second subarray.
  */
-
 void merge(int *array, int *auxiliary, size_t start, size_t mid, size_t end)
 {
 	size_t left = start, right = mid, merged_index = start;
 
-	printf("Merging...\n");
-	printf("[left]: ");
+	printf("Merging...\n[left]: ");
 	print_array(array + start, mid - start);
 	printf("[right]: ");
 	print_array(array + mid, end - mid);
@@ -26,36 +22,19 @@ void merge(int *array, int *auxiliary, size_t start, size_t mid, size_t end)
 	while (left < mid && right < end)
 	{
 		if (array[left] <= array[right])
-		{
-			auxiliary[merged_index] = array[left];
-			left++;
-		}
+			auxiliary[merged_index++] = array[left++];
 		else
-		{
-			auxiliary[merged_index] = array[right];
-			right++;
-		}
-		merged_index++;
+			auxiliary[merged_index++] = array[right++];
 	}
 
 	while (left < mid)
-	{
-		auxiliary[merged_index] = array[left];
-		left++;
-		merged_index++;
-	}
+		auxiliary[merged_index++] = array[left++];
 
 	while (right < end)
-	{
-		auxiliary[merged_index] = array[right];
-		right++;
-		merged_index++;
-	}
+		auxiliary[merged_index++] = array[right++];
 
 	for (merged_index = start; merged_index < end; merged_index++)
-	{
 		array[merged_index] = auxiliary[merged_index];
-	}
 
 	printf("[Done]: ");
 	print_array(array + start, end - start);
@@ -68,16 +47,12 @@ void merge(int *array, int *auxiliary, size_t start, size_t mid, size_t end)
  * @start: The starting index of the subarray.
  * @end: The ending index of the subarray.
  */
-
-
 void merge_sort_recursive(int *array, int *auxiliary, size_t start, size_t end)
 {
 	size_t mid;
 
 	if (end - start < 2)
-	{
 		return;
-	}
 
 	mid = start + (end - start) / 2;
 
@@ -96,17 +71,12 @@ void merge_sort(int *array, size_t size)
 	int *auxiliary_array;
 
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
 
 	auxiliary_array = malloc(sizeof(*auxiliary_array) * size);
 	if (auxiliary_array == NULL)
-	{
 		return;
-	}
 
 	merge_sort_recursive(array, auxiliary_array, 0, size);
-
 	free(auxiliary_array);
 }
